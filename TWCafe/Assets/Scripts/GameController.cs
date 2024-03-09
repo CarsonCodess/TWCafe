3b829882ea4a4eb1b7aa85dc7cba07ae13665f94
EliTE08 <memeinsta84@gmail.com>
2024-03-08 22:08:38 -0500

    Fixed Dash and Added Game Controller
    Fixed dash function to have a timer and also added an FPS regulator that is able to be adjusted.

--- TWCafe/Assets/Scripts/GameController.cs
+++ TWCafe/Assets/Scripts/GameController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    [SerializeField] int fps = 60;
     // Start is called before the first frame update
     void Start()
     {
-        Application.targetFrameRate = 60;
+        Application.targetFrameRate = fps;
     }
 
     // Update is called once per frame
--- TWCafe/Assets/Scripts/PlayerController.cs
+++ TWCafe/Assets/Scripts/PlayerController.cs
@@ -17,6 +17,8 @@ public class PlayerController : MonoBehaviour
     private InputAction _move;
     private InputAction _dash;
     private bool _canDash = true;
+    private float _dashTimer = 0;
+    private bool _isDashing = false;
     private Rigidbody2D _rb;
     private FoodItem _equippedItem;
 
@@ -47,11 +49,15 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         _moveDirection = _move.ReadValue<Vector2>();
+        _dashTimer += Time.deltaTime;
+        if(_dashTimer >= 4){
+            _canDash = true;
+        }
     }
 
     private void FixedUpdate()
     {
-        if(!_canDash)
+        if(_isDashing)
             return;
         var acceleration = (_moveDirection * moveSpeed - _rb.velocity) / 3f;
         _rb.velocity += acceleration * Time.deltaTime * 100;
@@ -61,10 +67,15 @@ public class PlayerController : MonoBehaviour
     {
         if(!_canDash)
             return;
-        _canDash = false;
-        var dashTarget = _rb.position + _moveDirection * dashDistance;
-        _rb.DOMove(dashTarget, dashTime).SetEase(Ease.InOutQuint);
-        _canDash = true;
+        if(_canDash && _dashTimer >= 4){
+            _isDashing = true;
+            var dashTarget = _rb.position + _moveDirection * dashDistance;
+            _rb.DOMove(dashTarget, dashTime).SetEase(Ease.InOutQuint).OnComplete (() => {
+                _isDashing = false;
+            });
+            _canDash = false;
+            _dashTimer = 0;
+        }
     }
 
     public void Pickup(FoodItem item)
