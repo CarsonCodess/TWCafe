1823d53c41b3bb511a5bbd41f97dde343f62caa0
Imated <lineborder122@gmail.com>
2024-04-08 18:14:09 -0400

    Asset PreProcessor
    Easier to import assets, auto sets settings

---
+++ TWCafe/Assets/Scripts/Utilities/SpritePostProcessor.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+public class SpritePostProcessor : AssetPostprocessor
+{
+    private void OnPreprocessTexture()
+    {
+        var lowerCaseAssetPath = assetPath.ToLower();
+        lowerCaseAssetPath = lowerCaseAssetPath.Replace("\\", "/");
+
+        var inSpriteDirectory = lowerCaseAssetPath.Contains("assets/textures/");
+        if(!inSpriteDirectory)
+            return;
+        var textureImporter = assetImporter as TextureImporter;
+        if (textureImporter != null)
+        {
+            textureImporter.textureType = TextureImporterType.Sprite;
+            textureImporter.textureCompression = TextureImporterCompression.Uncompressed;
+            textureImporter.filterMode = FilterMode.Point;
+        }
+    }
+}
